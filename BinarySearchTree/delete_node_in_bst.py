e6fdecad66f0ccfa9a48292b136d1a2af9893fe1
Ayan Jana <ayan.jana@oracle.com>
2022-08-10 03:41:02 +0530

    BST Practice

--- BinarySearchTree/delete_node_in_bst.py
+++ BinarySearchTree/delete_node_in_bst.py
@@ -51,3 +51,14 @@ class Solution:
             return None
         dfs(root, key)
         return root
+
+
+# Example1 :-
+
+# [44,11,45,7,21,null,49,6,8,13,24,47,null,0,null,null,9,12,19,23,25,46,48,null,2,null,10,null,null,15,20,22,null,null,38,null,null,null,null,1,3,null,null,14,16,null,null,null,null,33,41,null,null,null,5,null,null,null,18,29,34,40,43,4,null,17,null,27,30,null,36,39,null,42,null,null,null,null,null,26,28,null,31,35,37,null,null,null,null,null,null,null,null,null,32]
+# 49
+
+# Example2 :-
+
+# [15,12,22,10,13,16,25,6,11,null,14,null,17,23,29]
+# 15
