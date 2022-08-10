e62217010367985b343e664b49621a3e059c2b04
Ayan Jana <ayan.jana@oracle.com>
2022-08-11 03:38:00 +0530

    DP Practice

--- Dynamic Programming/delete_operations_of_two_strings.py
+++ Dynamic Programming/delete_operations_of_two_strings.py
@@ -22,7 +22,21 @@ class Solution:
                 dp[i][j] = 1 + min(f(i - 1, j), f(i, j - 1))
             return dp[i][j]
 
+        def f_td():
+            for i in range(m + 1):
+                for j in range(n + 1):
+                    if i == 0:
+                        dp[i][j] = j
+                    elif j == 0:
+                        dp[i][j] = i
+                    else:
+                        if word1[i - 1] == word2[j - 1]:
+                            dp[i][j] = dp[i - 1][j - 1]
+                        else:
+                            dp[i][j] = 1 + min(dp[i][j - 1], dp[i - 1][j])
+
         m, n = len(word1), len(word2)
         dp = [[-1] * (501) for _ in range(501)]
         f(m, n)
+        # f_td()
         return dp[m][n]
