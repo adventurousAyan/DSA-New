24727a7cbb10a37d35621e38ce7722e8eebb71d8
Ayan Jana <ayan.jana@oracle.com>
2022-08-24 19:14:32 +0530

    Arrangement

--- LinkedList/remove_nth_node_space_optimized.py
+++ LinkedList/remove_nth_node_space_optimized.py
@@ -14,18 +14,25 @@ class ListNode:
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
 
+        # Take a dummy node
         dummy = ListNode(0, head)
 
+        # Point nodes
         slow = dummy
         fast = head
 
+        # Iterate till n and move the fast pointer
         for _ in range(n):
             fast = fast.next
 
+        # Now move slo and fast both, till fast is null
         while fast:
             fast = fast.next
             slow = slow.next
 
+        # The moment fast reaches null, slow will be pointing to the prev node to be deleted.
+        # Delete the node by pointing slow next to slow next.next
         slow.next = slow.next.next
 
+        # Return dummy.next
         return dummy.next
