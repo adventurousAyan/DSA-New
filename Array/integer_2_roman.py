89711bef00501f5756ce1c908262fad3159ba6ce
Ayan Jana <ayan.jana@oracle.com>
2022-09-02 22:14:37 +0530

    DSA Practice

--- Array/integer_2_roman.py
+++ Array/integer_2_roman.py
@@ -1,8 +1,8 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-      
-        d1 = {}
 
+        d1 = {}
+        # Store all inum to  roman mappings in a dictionary. This will include the differencing as well
         d1[1] = "I"
         d1[4] = "IV"
         d1[5] = "V"
@@ -20,12 +20,15 @@ class Solution:
 
         def f(ls, num, ans):
 
+            # If Num is 0, we got our ans
             if num == 0:
                 return ans
+            # If number is a part of dictionary key, append it to ans and return
             if num in d1:
                 ans += d1[num]
                 return ans
 
+            # Do Binary Search
             start = 0
             end = len(ls) - 1
             floor = -1
@@ -36,11 +39,15 @@ class Solution:
                     start = mid + 1
                 elif num < ls[mid]:
                     end = mid - 1
+            # Get the floor value
             val = ls[floor]
+            # If number is greater than highest range
             if num > 1000:
                 ans += d1[1000] * (num // 1000)
                 num = num % 1000
             else:
+                # We need to divide the num by floor value and the quotient will be the number of roman
+                # characters needed for that value
                 ans += d1[val] * (num // val)
                 num = num % val
             return f(ls, num, ans)
