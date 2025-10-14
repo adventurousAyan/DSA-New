f3db4bbf12a0f9bf5ae65b56c930f7b7711587bb
ajana <ajana@salesforce.com>
2025-10-15 02:23:45 +0530

    practice 25

---
+++ dsa_practice_25/find_middle_index_array.py
@@ -0,0 +1,21 @@
+class Solution:
+    # https://leetcode.com/problems/find-the-middle-index-in-array/
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        pref_arr  = [0]*n
+        suff_arr = [0]*n
+        pref_arr[0] = nums[0]
+        suff_arr[n-1] = nums[n-1]
+        i, j, k = 1, n-2, 0
+        
+        while i < n and j >= 0:
+            pref_arr[i] = pref_arr[i-1] + nums[i]
+            suff_arr[j] = suff_arr[j+1] + nums[j]
+            i+=1
+            j-=1
+        while k < n:
+            if pref_arr[k] == suff_arr[k]:
+                return k
+            k+=1
+        return -1
+        
